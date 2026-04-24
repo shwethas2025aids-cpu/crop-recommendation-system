e7f303170767dc45390ccdd9f42c13d5881c9d88
shwetha <shwetha592007@gmail.com>
2026-04-25 01:04:31 +0530

    Change default port to 8080

--- app.py
+++ app.py
@@ -229,5 +229,5 @@ def server_error(error):
 # ─── RUN APP ──────────────────────────────────────────
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(debug=False, host="0.0.0.0", port=port)
